a4b4bb04503071dd583b7954424cbead3ef0cad1
Jose Ricardo Silveira <ricardusilveira@gmail.com>
2021-09-12 13:45:23 -0300

    Conversão não 100% completa, corrigir recursividade

--- script/conversao-jpg-png.sh
+++ script/conversao-jpg-png.sh
@@ -1,68 +1,72 @@
 #!/bin/bash
 
-# Criar uma estrutura de pastas igual ao lado da pasta a ser modificada. arquivo de log também ao lado
-# Verificar a possibilidade de colocar condição de caminho absoluto ou relativo a aplicação
 
 data_conversao=$(date '+%F')
 horario_conversao=$(date '+%2H:%2M:%S')
 
+
 percorre_arquivos_converte(){
     cd $1
-    diretorios_locais=()
     for arquivo in *;do
-        caminho_arquivo=$(find $DIRETORIO_ORIGINAL -name $arquivo)
+        local caminho_arquivo=$(find $DIRETORIO_ORIGINAL -name $arquivo)
         if [ -d $caminho_arquivo ];then
-            diretorios_locais+=($caminho_arquivo)
+            percorre_arquivos_converte $caminho_arquivo
         else
             if [[ $arquivo =~ \.jpg$ ]];then
-                converte_imagem $arquivo $caminho_arquivo
+                local caminho_relativo_imagem_diretorio_original=$(realpath --relative-to=$DIRETORIO_ORIGINAL \
+                    $caminho_arquivo)
+                converte_imagem $arquivo $caminho_relativo_imagem_diretorio_original
             fi
         fi
     done
-    for diretorio in $diretorios_locais;do
-        percorre_arquivos_converte $diretorio
-    done
-}
-# encontrar um jeito de pegar os nomes dos diretorios locais e empregá-los junto aos nomes das imagens para espelhar a
-# mesma estrutura de arquivos original
-converte_imagem(){
-    local $PNG/nome_imagem=$(ls $1 | awk -F. '{ print $1 }')
-    convert $2 $DIRETORIO_ORIGINAL/png/$nome_imagem.png
+
 }
 
+
+remove_extensao(){
+    local sem_extensao=$(echo $1 | awk -F. '{ printf $1 }')
+    echo $sem_extensao
+}
+
+
+converte_imagem(){
+    local nome_imagem=$(remove_extensao $1)
+    local caminho_relativo_imagem=$(remove_extensao $2)
+    convert $1 $DIRETORIO_PNG/$caminho_relativo_imagem.png
+}
+
+
+# Verificar a possibilidade de colocar condição de caminho absoluto ou relativo na aplicação
 DIRETORIO_ORIGINAL=$1
 
 cd $DIRETORIO_ORIGINAL
 find . -type d > dirs.txt
+# Programa não está convertendo todas as imagens. Provável que só as imagens cujas localizações se encontram ao lado do
+# diretório em que entra
 cd ..
-# aplicar comando case para substituir esses ifs abaixo
 if [ ! -d png ]
     then
         mkdir png
 fi
-if [ ! -d log ]
+DIRETORIO_PNG=$(realpath png)
+
+if [ ! -d logs ]
     then
-        mkdir log
+        mkdir logs
 fi
+DIRETORIO_LOGS=$(realpath logs)
 
-cd png
-PNG=realpath .
+echo $data_conversao,$horario_conversao >> $DIRETORIO_LOGS/log-conversao-$data_conversao.log
+
+cd $DIRETORIO_PNG
 xargs mkdir -p < $DIRETORIO_ORIGINAL/dirs.txt
-cd $DIRETORIO_ORIGINAL
-rm dirs.txt
+rm $DIRETORIO_ORIGINAL/dirs.txt
 
+percorre_arquivos_converte $DIRETORIO_ORIGINAL 2>> $DIRETORIO_LOGS/log-conversao-$data_conversao.log
 
-
-echo $data_conversao,$horario_conversao >> log/log-conversao-$data_conversao.log
-
-percorre_arquivos_converte $DIRETORIO_ORIGINAL 2>> log/log-conversao-$data_conversao.log
-
-cd $DIRETORIO_ORIGINAL
-
-if [ $? -eq 0 ]
-    then
-        echo "Conversão realizada com sucesso" >> log/log-conversao-$data_conversao.log
-        echo "Conversão realizada com sucesso"
+if [ $? -eq 0 ];then
+    echo "Conversão realizada com sucesso" >> $DIRETORIO_LOGS/log-conversao-$data_conversao.log
 else
-    echo "Houve uma falha no processo de conversão. Por favor, verifique o arquivo de log em $DIRETORIO_ORIGINAL/../log"
+    echo "Houve uma falha no processo de conversão. Por favor, verifique o arquivo de log em $DIRETORIO_LOGS/\
+    log-conversao-$data_conversao.log"
 fi
