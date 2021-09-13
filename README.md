453a389c348fb84116b792e6ca024c8f384125c1
José Ricardo Silveira <39640804+Zericardos@users.noreply.github.com>
2021-09-12 21:41:55 -0300

    Dev (#1)
    * Conversor png funcionando
    
    * COMMENT correção de comentários

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # scripts-bash
-Scripts bash para automatizacao de tarefas
+Scripts bash para automação de tarefas
+
+## Conversão <img src="scripts/images/jpg-to-png.png" alt="jpg-to-png" width="80"/>
+
+Converte todas as imagens no formato *jpg* em formato *png*
+
+### Comando da aplicação
+
+**bash conversao-jpg-png <\caminho absoluto do diretório a ser convertido\>**
+
+*Exemplo geral: bash conversao-jpg-png $(realpath <\caminho relativo\>)*
+
+*Exemplo específico na pasta do projeto:*
+
+*bash scripts/conversao-jpg-png.sh $(realpath imagens-novos-livros/)*
+
+- É necessário que cada imagem *jpg* tenha extensão *.jpg*
+- Cria uma cópia com a estrutura de diretórios internos ao diretório pai com as imagens convertidas além de um diretório *logs*
+- Cria arquivo de log na pasta logs para cada dia que o processo é executado no formato *conversao-YYYY-MM-DD.log*
+    - *Exemplo*: O arquivo **conversao-2021-09-12.log** contém a mensagem final de todas as execuções realizadas na data apresentada.
+    - Cada execução da aplicação é registrada como a data e hora de execução no arquivo de log
+    - Caso ocorra algum erro na conversão, é mostrada a razão.
+- Mostra-se uma mensagem final de sucesso ou erro na conversão.
---
+++ scripts/conversao-jpg-png.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+
+data_conversao=$(date '+%F')
+horario_conversao=$(date '+%2H:%2M:%S')
+
+
+percorre_arquivos_converte(){
+    cd $1
+    for arquivo in *;do
+        local caminho_arquivos=$(find $DIRETORIO_ORIGINAL -name $arquivo)
+        for caminho_arquivo in $caminho_arquivos;do
+            if [ -d $caminho_arquivo ];then
+                percorre_arquivos_converte $caminho_arquivo
+            else
+                if [[ $arquivo =~ \.jpg$ ]];then
+                    local caminho_relativo_imagem_diretorio_original=$(realpath --relative-to=$DIRETORIO_ORIGINAL \
+                        $caminho_arquivo)
+                    converte_imagem $caminho_arquivo $caminho_relativo_imagem_diretorio_original
+                fi
+            fi
+        done
+    done
+
+}
+
+
+remove_extensao(){
+    local sem_extensao=$(echo $1 | awk -F. '{ printf $1 }')
+    echo $sem_extensao
+}
+
+
+converte_imagem(){
+    local caminho_relativo_imagem=$(remove_extensao $2)
+    convert $1 $DIRETORIO_PNG/$caminho_relativo_imagem.png
+    }
+
+verifica_cria_diretorio(){
+    if [ ! -d $1 ]; then
+        mkdir $1
+    fi
+}
+
+# Verificar a possibilidade de colocar condição de caminho absoluto ou relativo na aplicação
+DIRETORIO_ORIGINAL=$1
+
+cd $DIRETORIO_ORIGINAL
+find . -type d > dirs.txt  # imprime os caminhos relativos dos diretórios e subdiretórios ao diretório original no
+# arquivo dirs.txt
+
+cd ..
+verifica_cria_diretorio png
+DIRETORIO_PNG=$(realpath png)
+
+verifica_cria_diretorio logs
+DIRETORIO_LOGS=$(realpath logs)
+
+echo $data_conversao,$horario_conversao >> $DIRETORIO_LOGS/conversao-$data_conversao.log
+
+cd $DIRETORIO_PNG
+xargs mkdir -p < $DIRETORIO_ORIGINAL/dirs.txt
+rm $DIRETORIO_ORIGINAL/dirs.txt
+
+percorre_arquivos_converte $DIRETORIO_ORIGINAL 2>> $DIRETORIO_LOGS/conversao-$data_conversao.log
+
+if [ $? -eq 0 ];then
+    echo "Conversão realizada com sucesso" >> $DIRETORIO_LOGS/conversao-$data_conversao.log
+    echo "Conversão realizada com sucesso"
+else
+    echo "Houve uma falha no processo de conversão. Por favor, verifique o arquivo de log em $DIRETORIO_LOGS/\
+    conversao-$data_conversao.log"
+fi
